5e4949b91f85e02c4b3d7c37b074a24a64f7f461
itisnotyourenv <64085243+itisnotyourenv@users.noreply.github.com>
2023-09-20 23:08:32 +0300

    Refactoring (#5)
    * deleted some unused files
    
    * mini refactoring
    
    * updated README.md
    
    * divided logic in task

--- image_generator/tasks/image_generation.py
+++ image_generator/tasks/image_generation.py
@@ -2,6 +2,7 @@ import logging
 
 from aiogram import Bot
 from aiogram.types import FSInputFile
+from aiogram.exceptions import TelegramBadRequest
 
 from image_generator.builder import generate_image
 from tgbot.config import load_config
@@ -12,10 +13,36 @@ bot = Bot(token=config.tg_bot.token)
 
 
 async def start_image_generation(ctx: dict, user_id: int, text: str):
+    """
+    Generate image and send it to user
+
+    :param ctx: Context from arq.
+    :param user_id: User id to send image.
+    :param text: Text to generate image.
+    :return: None
+    """
     logging.info("Start image generation for user %s", user_id)
-    path = generate_image(text)
+    try:
+        path = generate_image(text)
+        await send_image(user_id, path, caption=text)
+    except FileNotFoundError:
+        logging.exception("Error while generating image for user %s", user_id)
+        return
     logging.info("Generated image for user %s saved to %s", user_id, path)
 
-    photo = FSInputFile(path)
-    # todo - catch exceptions
-    await bot.send_photo(user_id, photo)
+
+async def send_image(user_id: int, image_path: str, caption: str = None):
+    """
+    Send image to user
+
+    :param user_id: User id to send image.
+    :param caption: Caption for image.
+    :param image_path: Path to image.
+    :return: None
+    """
+    logging.info("Sending image to user %s", user_id)
+    photo = FSInputFile(image_path)
+    try:
+        await bot.send_photo(user_id, photo, caption=caption)
+    except TelegramBadRequest:
+        logging.exception("Error while sending image to user %s", user_id)
