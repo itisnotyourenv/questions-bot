c6d7cc9abfc69f459002e687e438ae9820e631d5
itisnotyourenv <64085243+itisnotyourenv@users.noreply.github.com>
2024-01-13 11:06:26 +0400

    create stats command (#12)
    * create stats command
    
    * change User >> self.model in UserRepo

--- infrastructure/database/repo/questions.py
+++ infrastructure/database/repo/questions.py
@@ -1,6 +1,15 @@
 from infrastructure.database.models import Question
 from infrastructure.database.repo.base import BaseRepo
 
+from sqlalchemy import select, func
+
 
 class QuestionsService(BaseRepo):
     model = Question
+
+    async def count(self):
+        """
+        Returns the number of users in the database.
+        :return: The number of users in the database.
+        """
+        return await self.session.scalar(select(func.count(self.model.question_id)))
--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -1,16 +1,22 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import Command
 from aiogram.types import Message
-from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from tgbot.filters.admin import AdminFilter
-from infrastructure.database.models import User
 from infrastructure.database.repo.requests import RequestsRepo
 
 admin_router = Router()
 admin_router.message.filter(AdminFilter())
 
 
-@admin_router.message(CommandStart())
-async def admin_start(message: Message, session: AsyncSession, user: User, repo: RequestsRepo):
-    await message.answer("tamam")
+@admin_router.message(Command("stats"))
+async def admin_start(message: Message, repo: RequestsRepo):
+    users_count = await repo.users.count()
+    questions_count = await repo.questions.count()
+
+    text = (
+        "📊 Небольшая статистика:\n\n"
+        f"👤 Пользователей: {users_count}\n"
+        f"❓ Вопросов: {questions_count}\n"
+    )
+    await message.answer(text)
